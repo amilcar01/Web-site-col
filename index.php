77892f129bf81cb4be5daaeaf2294e2be4864074
richysoft2021 <richard.perez@uab.edu.bo>
2021-08-15 16:57:35 -0400

    modificacion
    modificacion texto

--- index.php
+++ index.php
@@ -1,91 +1,38 @@
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta charset="UTF-8"/>
-    <link href='img/logo.png' rel='shortcut icon' type='image/png'/>
-    
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+<?php 
+include_once ("incluciones/header.php");
+include_once ("incluciones/carousel.php");
+?> <br>
 
-    <title>U.E. SOCAVON</title>
-  </head>
-  <body>
-    
-
-    <!---   menu   --->
-    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-      <img src="img/logo.png" width="40px">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="index.php">U.E. Nuetra Sra. de Socavon</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarColor01">
-          <ul class="navbar-nav me-auto">
-            <li class="nav-item">
-              <a class="nav-link" href="actividades.php">Actividades</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="plantelDocente.php">Plantel Docente</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="galeriaImg.php">Imagenes</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="historia.php">Historia</a>
-            </li>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-              <div class="dropdown-menu">
-                <a class="dropdown-item" href="#">Action</a>
-                <a class="dropdown-item" href="#">Another action</a>
-                <a class="dropdown-item" href="#">Something else here</a>
-                <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="#">Separated link</a>
-              </div>
-            </li>
-          </ul>
-          <form class="d-flex">
-            <input class="form-control me-sm-2" type="text" placeholder="Buscar">
-            <button class="btn btn-secondary my-2 my-sm-0" type="submit">Buscar...</button>
-          </form>
-        </div>
-      </div>
-    </nav>
-
-    <!------menu-------->
-
-    <div class="container">
-      <div class="row">
-        <div align="justify">
-          
-          <div class="list-group">
-            <a href="#" class="list-group-item list-group-item-action flex-column align-items-start active">
-              <div class="d-flex w-100 justify-content-between">
-                <h3>Misión</h3>
-              </div>
-            </a>
-            <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
-              <p>Fomentar la educación formal y los hábitos sociales y de convivencia adecuados. · Inculcar el respeto hacia el entorno, material y las instalaciones del colegio. · Desarrollar en sus alumnos la autoconfianza.</p>
-            </a>
-          </div> <br>
-
-          <div class="list-group">
-            <a href="#" class="list-group-item list-group-item-action flex-column align-items-start active">
-              <div class="d-flex w-100 justify-content-between">
-                <h3>Visión</h3>
-              </div>
-            </a>
-            <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
-              <p>Un Colegio que favorezca la formación integral del alumnado contando con su diversidad, fomentando el espíritu de superación, cooperación, respeto, solidaridad, creatividad y sentido de la trascendencia. ... Un Colegio plurilingüe que sea reconocido en nuestro entorno por una enseñanza de calidad en los idiomas.</p>
-            </a>
+<div class="container">
+  <div class="row">
+    <div align="justify">
+      
+      <div class="list-group">
+        <a href="#" class="list-group-item list-group-item-action flex-column align-items-start active">
+          <div class="d-flex w-100 justify-content-between">
+            <h3>Misión</h3>
           </div>
+        </a>
+        <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
+          <p>Fomentar la educación formal y los hábitos sociales y de convivencia adecuados. · Inculcar el respeto hacia el entorno, material y las instalaciones del colegio. · Desarrollar en sus alumnos la autoconfianza.</p>
+        </a>
+      </div> <br>
 
-        </div>
+      <div class="list-group">
+        <a href="#" class="list-group-item list-group-item-action flex-column align-items-start active">
+          <div class="d-flex w-100 justify-content-between">
+            <h3>Visión</h3>
+          </div>
+        </a>
+        <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
+          <p>Un Colegio que favorezca la formación integral del alumnado contando con su diversidad, fomentando el espíritu de superación, cooperación, respeto, solidaridad, creatividad y sentido de la trascendencia. ... Un Colegio plurilingüe que sea reconocido en nuestro entorno por una enseñanza de calidad en los idiomas.</p>
+        </a>
       </div>
-    </div>
 
-  </body>
-</html>
+    </div>
+  </div>
+</div>
+
+<?php 
+include_once ("incluciones/footer.php");
+?>
