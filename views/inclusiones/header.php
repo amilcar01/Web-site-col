e0a7c0dd2fe191a5a404aab56efbd03b5420d28e
amilcar01 <88783796+amilcar01@users.noreply.github.com>
2021-08-16 17:08:05 -0400

    quirar dropdown

--- views/inclusiones/header.php
+++ views/inclusiones/header.php
@@ -9,11 +9,6 @@
 
         <div class="collapse navbar-collapse" id="navbarColor01">
           <ul class="navbar-nav me-auto">
-            <!--<li class="nav-item">
-              <a class="nav-link active" href="#">Home
-                <span class="visually-hidden">(current)</span>
-              </a>
-            </li> -->
             <li class="nav-item">
               <a class="nav-link" href="../actividades.php">Actividades</a>
             </li>
@@ -25,16 +20,6 @@
             </li>
             <li class="nav-item">
               <a class="nav-link" href="historia.php">Historia</a>
-            </li>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-              <div class="dropdown-menu">
-                <a class="dropdown-item" href="#">Action</a>
-                <a class="dropdown-item" href="#">Another action</a>
-                <a class="dropdown-item" href="#">Something else here</a>
-                <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="#">Separated link</a>
-              </div>
             </li>
           </ul>
           <form class="d-flex">
