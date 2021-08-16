64faa3d7d17548e08594086ea73fad204af7750c
richysoft2021 <richard.perez@uab.edu.bo>
2021-08-15 21:34:16 -0400

    edicion bodiy
    adicion contenido body actividades

--- actividades.php
+++ actividades.php
@@ -1,3 +1,71 @@
 <?php
 include_once ("inclusiones/header.php");
 ?>
+<center><h1>Actividades Unidad Educativa "Nuestra Sra. del Socavon"</h1></center>
+
+<div class="container">
+	<div class="row">
+		<div class="card mb-3">
+		  <h3 class="card-header">6 de agosto</h3>
+		  <!--<div class="card-body">
+		    <h5 class="card-title">Special title treatment</h5>
+		    <h6 class="card-subtitle text-muted">Support card subtitle</h6> 
+		  </div> -->
+		  <!--<svg xmlns="http://www.w3.org/2000/svg" class="d-block user-select-none" width="100%" height="200" aria-label="Placeholder: Image cap" focusable="false" role="img" preserveAspectRatio="xMidYMid slice" viewBox="0 0 318 180" style="font-size:1.125rem;text-anchor:middle">
+		    <rect width="100%" height="100%" fill="#868e96"></rect> 
+		  </svg>-->
+
+		  <img src="img/1.jpg">
+		  <div class="card-body">
+		    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+		  </div>
+		  
+		  <div class="card-footer text-muted">
+		    2 days ago
+		  </div>
+		</div>
+		
+	</div>
+</div>
+
+<div class="container">
+	<div class="row">
+		<div class="col-md-4">
+			<div class="list-group">
+			  <a href="#" class="list-group-item list-group-item-action active">Cras justo odio</a>
+			  <img src="img/logo.png">
+			  <a href="#" class="list-group-item list-group-item-action disabled">Morbi leo risus</a>
+			</div>
+		</div>
+		<div class="col-md-4">
+			<div class="list-group">
+			  <a href="#" class="list-group-item list-group-item-action active">Cras justo odio</a>
+			  <img src="img/logo.png">
+			  <a href="#" class="list-group-item list-group-item-action disabled">Morbi leo risus</a>
+			</div>
+		</div>
+		<div class="col-md-4">
+			<div class="list-group">
+			  <a href="#" class="list-group-item list-group-item-action active">Cras justo odio</a>
+			  <img src="img/logo.png">
+			  <a href="#" class="list-group-item list-group-item-action disabled">Morbi leo risus</a>
+			</div>
+		</div>
+	</div>
+</div> <br>
+
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-md-12">
+			<div class="list-group">
+			  <a href="#" class="list-group-item list-group-item-action active">Cras justo odio</a>
+			  
+			  <a href="#" class="list-group-item list-group-item-action disabled">Morbi leo risus</a>
+			</div>
+		</div>
+	</div>
+</div>
+
+<?
+include_once ("inclusiones/footer.php");    
+?>
