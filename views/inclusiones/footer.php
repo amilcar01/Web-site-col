701ba6e8b2156bb8eca56c4daca0d10d8e22ebd7
Rolando Pardo Terrazas <rolando.pardo@uab.edu.bo>
2021-08-16 20:29:37 -0400

    redes sociales
    implementaciones de redes sociales en el footer

--- views/inclusiones/footer.php
+++ views/inclusiones/footer.php
@@ -1,8 +1,16 @@
-
+<br>
 <div class="container-fluid">
     <div class="row">
         <div class="col">
-            <p>Unidad Educativa Nuestra señora del Socavon</p>
+            <center>
+                <label>Unidad Educativa Nuestra señora del Socavon</label>
+                <div class="sb-sidenav-footer">
+                    <div class="small">
+                        Facebook : <a href="facebook.com"><img src="views/img/logo_face.png" width="30"></a>
+                        WhatsApp:  <a href="https://api.whatsapp.com/send?phone=59167545540"><img src="views/img/logo_what.png" width="30"></a>
+                    </div>
+                </div>
+            </center>
         </div>
     </div>
 </div>
