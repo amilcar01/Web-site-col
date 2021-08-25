251f8b151f00c38a4909015f69fc484a3bcfec74
Rolando <rolando.pardo@uab.edu.bo>
2021-08-25 11:41:08 -0400

    actualizacion de plantel docente
    actualizacion de plantel administrativo, profesores de nivel primario y profesores de nivel secundaria

--- views/inclusiones/plantel.php
+++ views/inclusiones/plantel.php
@@ -2,42 +2,107 @@
 <div class="row">
 	<center>
 		<div class="col-md-10" >
-			<table class="table table-hover">
-			  <thead class="table-light">
-			    <tr>
-			      <th scope="col">#</th>
-			      <th scope="col">Nombre </th>
-			      <th scope="col">Cargo</th>
-			      <th scope="col">Celular</th>
-			    </tr>
-			  </thead>
-			  <tbody>
-			    <tr>
-			      <th scope="row">1</th>
-			      <td>Juan Jose Colque</td>
-			      <td>Director</td>
-			      <td>7556323</td>
-			    </tr>
-			    <tr>
-			      <th scope="row">2</th>
-			      <td>Roberto Santos Mendez</td>
-			      <td>prof. Quimica</td>
-			      <td>726362</td>
-			    </tr>
-			    <tr>
-			      <th scope="row">3</th>
-			      <td>Omar Claros Rojas</td>
-			      <td>prof. Fisica</td>
-			      <td>7263332</td>
-			    </tr>
-			    <tr>
-			      <th scope="row">4</th>
-			      <td>Juan Jose Colque</td>
-			      <td>prof. Matematicas</td>
-			      <td>7556323</td>
-			    </tr>
-			  </tbody>
-			</table>
+			<H4>Plantel Administrativo</H4>
+				<table class="table table-hover">
+				  <thead class="table-dark">
+				    <tr>
+				      <th scope="col">#</th>
+				      <th scope="col">CARGO</th>
+				      <th scope="col">PERSONAL</th>
+				      
+				    </tr>
+				  </thead>
+				  <tbody>
+				    <tr>
+				      <th scope="row">1</th>
+				      <td>Director</td>
+				      <td>Juan Jose Colque</td>
+				      
+				    </tr>
+				    <tr>
+				      <th scope="row">2</th>
+				      <td>Secretaria</td>
+				      <td>Lidea Mendez</td>
+				    </tr>
+				    <tr>
+				      <th scope="row">3</th>
+				      <td>Portero</td>
+				      <td>Omar Claros Rojas</td>
+				      
+				    </tr>
+				   
+				  </tbody>
+				</table>
+			<H4>Profesores de Primaria </H4>
+				<table class="table table-hover">
+				  <thead class="table-dark">
+				    <tr>
+				      <th scope="col">#</th>
+				      <th scope="col">MATERIA</th>
+				      <th scope="col">DOCENTE</th>
+				      <th scope="col">CURSO</th>
+				    </tr>
+				  </thead>
+				  <tbody>
+				    <tr>
+				      <th scope="row">1</th>
+				      <td rowspan="2">Matematicas</td>
+				      <td>Juan Jose Colque</td>
+				      <td>1ro a 3ro de secundaria</td>
+				    </tr>
+				    <tr>
+				      <th scope="row">2</th>
+				      <td>Roberto Santos Mendez</td>
+				      <td>4to a 6to de secundaria</td>
+				    </tr>
+				    <tr>
+				      <th scope="row">3</th>
+				      <td rowspan="2">Lenguaje</td>
+				      <td>Omar Claros Rojas</td>
+				      <td>1ro a 3ro de secundaria</td>
+				    </tr>
+				    <tr>
+				      <th scope="row">4</th>
+				      <td>Juan Jose Colque</td>
+				      <td>4to a 6to de secundaria</td>
+				    </tr>
+				  </tbody>
+				</table>
+			<H4>Profesores de Secundario</H4>
+				<table class="table table-hover">
+				  <thead class="table-dark">
+				    <tr>
+				      <th scope="col">#</th>
+				      <th scope="col">MATERIA</th>
+				      <th scope="col">DOCENTE</th>
+				      <th scope="col">CURSO</th>
+				    </tr>
+				  </thead>
+				  <tbody>
+				    <tr>
+				      <th scope="row">1</th>
+				      <td rowspan="2">Matematicas</td>
+				      <td>Juan Jose Colque</td>
+				      <td>1ro a 3ro de secundaria</td>
+				    </tr>
+				    <tr>
+				      <th scope="row">2</th>
+				      <td>Roberto Santos Mendez</td>
+				      <td>4to a 6to de secundaria</td>
+				    </tr>
+				    <tr>
+				      <th scope="row">3</th>
+				      <td rowspan="2">Quimica</td>
+				      <td>Omar Claros Rojas</td>
+				      <td>1ro a 3ro de secundaria</td>
+				    </tr>
+				    <tr>
+				      <th scope="row">4</th>
+				      <td>Juan Jose Colque</td>
+				      <td>4to a 6to de secundaria</td>
+				    </tr>
+				  </tbody>
+				</table>
 		</div>
 	</center>
 </div>
