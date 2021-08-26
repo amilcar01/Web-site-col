6203b1909ff20608bc4b8c475f5ab4d90f0be8b0
arnoldperma <arnold.perez@uab.edu.bo>
2021-08-26 09:50:58 -0400

    template
    llamar actividades

--- views/inclusiones/actividades.php
+++ views/inclusiones/actividades.php
@@ -1,83 +1,38 @@
-
-<!--<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="@mdo">Open modal for @mdo</button>
-<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="@fat">Open modal for @fat</button>
-<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="@getbootstrap">Open modal for @getbootstrap</button> -->
-
-<div class="modal fade " id="actividades" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h4 class="modal-title" id="exampleModalLabel">Actividades de la Unidad Educativa </h4>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-        
-
-		<div class="container">
-			<div class="row">
-				<div class="card mb-3">
-				  <h5 class="card-header">6 de agosto</h5>
-				  <img src="views/img/1.jpg">
-				  <div class="card-body">
-				    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-				  </div>
-				  
-				  <div class="card-footer text-muted">
-				    2 days ago
-				  </div>
-				</div>
-				
-			</div>
-		</div>
-
-		<div class="container">
-			<div class="row">
-				<div class="col-md-6">
-					<div class="list-group">
-					  <a href="#" class="list-group-item list-group-item-action active">Cras justo odio</a>
-					  <img src="views/img/logo.png">
-					  <a href="#" class="list-group-item list-group-item-action disabled">Morbi leo risus</a>
-					</div>
-				</div>
-				<div class="col-md-6">
-					<div class="list-group">
-					  <a href="#" class="list-group-item list-group-item-action active">Cras justo odio</a>
-					  <img src="views/img/logo.png">
-					  <a href="#" class="list-group-item list-group-item-action disabled">Morbi leo risus</a>
-					</div>
-				</div>
-				
-			</div>
-		</div> <br>
-
-		<div class="container-fluid">
-			<div class="row">
-				<div class="col-md-12">
-					<div class="list-group">
-					  <a href="#" class="list-group-item list-group-item-action active">Cras justo odio</a>
-					  
-					  <a href="#" class="list-group-item list-group-item-action disabled">Morbi leo risus</a>
-					</div>
-				</div>
-			</div>
-		</div>
-		</div>
-		<div class="modal-footer">
-			<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
-			<!--<button type="button" class="btn btn-primary">Send message</button> -->
-		</div>
+<!-- galeria -->
+<div class="container">
+  <div class="row align-items-start">
+    <div class="col">
+      <h5 class="text-center">AÑO DEPORTIVO</h5>
+        <p class="text-justify">El colegio trabaja dos grandes áreas de importancia para el desarrollo integral de nuestra comunidad estudiantil: la actividad física y el deporte. La primera en base a un programa de estimulación de ejercicios para el mantenimiento del buen estado físico (aspectos no competitivos). El deporte, a través de procesos organizados de detección de talentos, y la planificación deportiva mediante ciclos de entrenamiento (aspectos competitivos). Todo se inicia a partir de un significativo evento a través del cual se inaugura el “Año Deportivo”, recordando los aspectos disciplinarios y éticos que deben mantenerse vigentes durante todo el año.</p>
     </div>
   </div>
-</div>
-			
+  <div class="row align-items-center">
+    <div class="col-lg-4 col-md-12 mb-4 mb-lg-0">
+      <img
+			  src="views/actividades/deportivo1.jpg"
+        class="w-100 shadow-1-strong rounded mb-4"
+        class="rounded"
+			  alt="Eniun"
+			/>
+    </div>
+    <div class="col-lg-4 col-md-12 mb-4 mb-lg-0">
+      <img
+			  src="views/actividades/deportivo2.jpg"
+			  class="w-100 shadow-1-strong rounded mb-4"
+        class="rounded"
+			  alt=""
+			/>
+    </div>
+    <div class="col-lg-4 col-md-12 mb-4 mb-lg-0">
+      <img
+			  src="views/actividades/deportivo3.jpg"
+			  class="w-100 shadow-1-strong rounded mb-4"
+			  alt=""
+			/>
+    </div>
+  </div>
+
+    <hr class="featurette-divider">
 
 
-
-
-
-
-
-
-
- 	
-
+<!-- galeria -->
