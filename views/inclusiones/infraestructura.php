afdd00b44fd973ce7fb62df2bfe72fc31984e361
richysoft2021 <88849943+richysoft2021@users.noreply.github.com>
2021-08-26 14:29:01 -0400

    segundo carousel
    Adicion segundo carousel

--- views/inclusiones/infraestructura.php
+++ views/inclusiones/infraestructura.php
@@ -28,6 +28,13 @@
 						<p>Frontis Unidad Educativa Nuestra Señora del Socavon</p>
 					</div>
 				</div>
+				<div class="carousel-item">
+					  <img src="views/img/sala.jpg" class="d-block" alt="..." width="400" height="350">
+					  <div class="carousel-caption d-md-block bg-info">
+						<h5>SALA DE COMPUTACION</h5>
+						<p>Ambientes equipados con sala de computacion.</p>
+					  </div>
+					</div>
 			</div>
 		</div>
     </div>
