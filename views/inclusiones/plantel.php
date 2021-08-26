dfeaa9a7dcc3c8427b9af74c077f457c0530a809
Rolando <rolando.pardo@uab.edu.bo>
2021-08-26 17:43:16 -0400

    corrección de plantel docente
    corrección de plantel docente

--- views/inclusiones/plantel.php
+++ views/inclusiones/plantel.php
@@ -1,138 +1,141 @@
-<!----plantel administrativo----->
-<div class="container-fluid">
-	<div class="row">
-		<div class="col-md-6">
-			<img src="views/img/admin.jpg">
-		</div>
-		<div class="col-md-6">
-			<center><H4 >Plantel Administrativo</H4></center>
-			<table class="table table-hover">
-			  <thead class="table-dark">
-			    <tr>
-			      <th scope="col">#</th>
-			      <th scope="col">CARGO</th>
-			      <th scope="col">PERSONAL</th>
-			      
-			    </tr>
-			  </thead>
-			  <tbody>
-			    <tr>
-			      <th scope="row">1</th>
-			      <td>Director</td>
-			      <td>Juan Jose Colque</td>
-			      
-			    </tr>
-			    <tr>
-			      <th scope="row">2</th>
-			      <td>Secretaria</td>
-			      <td>Lidea Mendez</td>
-			    </tr>
-			    <tr>
-			      <th scope="row">3</th>
-			      <td>Portero</td>
-			      <td>Omar Claros Rojas</td>
-			      
-			    </tr>
-			   
-			  </tbody>
-			</table>
-		</div>
-	</div>
-</div>
-<br><br>
-<!----plantel administrativo----->			
+<section id="plantel" style="background-color: #F2F2F2;"><br><br><br>
+    <div class="container wow fadeInUp">
+      <div class="row">
+        <div class="col-md-12">
+          <hr class="featurette-divider">
+          <h2>Plantel Docente</h2>
+        </div>
+      </div>
+    </div>
+    <div class="container about-container wow fadeInUp">
+    	<!--------plantel administrativo-------->
+		<div class="row" align="center">
+			<div class="col-md-12">
+				<h3 align="center">Plantel Administrativo</h3>
+			</div><br><br>
+			<div class="col-md-4"><br><br><br><br>
+				
+			</div>
+			<div class="col-md-4">
+			<div class="card" style="width: 18rem;">
+			  	<img src="views/img/dir.jpg" class="card-img-top" alt="..." >
+			  	<div class="card-body" align="center">
+			    	<h5 class="card-title">Director</h5>
+			    	<p class="card-text">Juan Jose Colque</p>
+			  	</div>
+			</div>
+			  
+			</div>
+			<div class="col-md-4">
+			</div>
+		</div><br>
+		<div class="row" align="center">
+			<div class="col-lg-6">
+				<div class="card" style="width: 18rem;">
+				  	<img src="views/img/secre.jpg" class="card-img-top" alt="...">
+				  	<div class="card-body">
+					    <h5 class="card-title">Secretariar</h5>
+					    <p class="card-text">Luz Maria Rojas</p>
+				  	</div>
+				</div>
+			</div>
+			<div class="col-lg-6">
+				<div class="card" style="width: 18rem;">
+				  	<img src="views/img/portera.jpg" class="card-img-top" alt="...">
+				  	<div class="card-body">
+				    	<h5 class="card-title">Portera</h5>
+				    	<p class="card-text">Juan Pillco Fuentes</p>
+				  	</div>
+				</div>
+			</div>
 			
-<!----profesores primaria----->
-<div class="container-fluid">
-	<div class="row">
-		<div class="col-md-6">
-			<center><H4>Profesores de Primaria </H4></center>
-			<table class="table table-hover">
-			  <thead class="table-dark">
-			    <tr>
-			      <th scope="col">#</th>
-			      <th scope="col">MATERIA</th>
-			      <th scope="col">DOCENTE</th>
-			      <th scope="col">CURSO</th>
-			    </tr>
-			  </thead>
-			  <tbody>
-			    <tr>
-			      <th scope="row">1</th>
-			      <td rowspan="2">Matematicas</td>
-			      <td>Juan Jose Colque</td>
-			      <td>1ro a 3ro de secundaria</td>
-			    </tr>
-			    <tr>
-			      <th scope="row">2</th>
-			      <td>Roberto Santos Mendez</td>
-			      <td>4to a 6to de secundaria</td>
-			    </tr>
-			    <tr>
-			      <th scope="row">3</th>
-			      <td rowspan="2">Lenguaje</td>
-			      <td>Omar Claros Rojas</td>
-			      <td>1ro a 3ro de secundaria</td>
-			    </tr>
-			    <tr>
-			      <th scope="row">4</th>
-			      <td>Juan Jose Colque</td>
-			      <td>4to a 6to de secundaria</td>
-			    </tr>
-			  </tbody>
-			</table>
-		</div>
-		<div class="col-md-6">
-			<img src="views/img/primaria.jpg">
-		</div>
-	</div>
-</div> <br><br>
-<!----profesores primaria----->			
-
-			
-<!----prof. secundaria----->
-<div class="container-fluid">
-	<div class="row">
-		<div class="col-md-6">
-			<img src="views/img/secundaria.jpg">
-		</div>
-		<div class="col-md-6">
-			<center><H4>Profesores de Secundario</H4></center>
-			<table class="table table-hover">
-			  <thead class="table-dark">
-			    <tr>
-			      <th scope="col">#</th>
-			      <th scope="col">MATERIA</th>
-			      <th scope="col">DOCENTE</th>
-			      <th scope="col">CURSO</th>
-			    </tr>
-			  </thead>
-			  <tbody>
-			    <tr>
-			      <th scope="row">1</th>
-			      <td rowspan="2">Matematicas</td>
-			      <td>Juan Jose Colque</td>
-			      <td>1ro a 3ro de secundaria</td>
-			    </tr>
-			    <tr>
-			      <th scope="row">2</th>
-			      <td>Roberto Santos Mendez</td>
-			      <td>4to a 6to de secundaria</td>
-			    </tr>
-			    <tr>
-			      <th scope="row">3</th>
-			      <td rowspan="2">Quimica</td>
-			      <td>Omar Claros Rojas</td>
-			      <td>1ro a 3ro de secundaria</td>
-			    </tr>
-			    <tr>
-			      <th scope="row">4</th>
-			      <td>Juan Jose Colque</td>
-			      <td>4to a 6to de secundaria</td>
-			    </tr>
-			  </tbody>
-			</table>
-		</div>
-	</div>
-</div><br><br>
-<!----prof. secundaria----->
+		</div><br><br>
+		<!--------plantel administrativo-------->
+		<!--------prof. primaria-------->
+		<div class="row" align="center">
+			<div class="col-md-12">
+				<h3 align="center">Profesores nivel Primaria</h3><br>
+			</div>
+			<div class="col-lg-3">
+				<div class="card" style="width: 18rem;">
+				  	<img src="views/img/pri1.jpg" class="card-img-top" alt="...">
+				  	<div class="card-body">
+					    <h5 class="card-title">Prof. Matemáticas</h5>
+					    <p class="card-text">Ronal Jose Colque</p>
+				  	</div>
+				</div>
+			</div>
+			<div class="col-lg-3">
+				<div class="card" style="width: 18rem;">
+				  	<img src="views/img/pri2.jpg" class="card-img-top" alt="...">
+				  	<div class="card-body">
+				    	<h5 class="card-title">Prof. Lenguaje</h5>
+				    	<p class="card-text">Mirian Fuentes</p>
+				  	</div>
+				</div>
+			</div>
+			<div class="col-lg-3">
+				<div class="card" style="width: 18rem;">
+				  	<img src="views/img/pri3.jpg" class="card-img-top" alt="...">
+				  	<div class="card-body">
+				    	<h5 class="card-title">Prof. Manualidades</h5>
+				    	<p class="card-text">Omar Claros</p>
+				  	</div>
+				</div>
+			</div>
+			<div class="col-lg-3">
+				<div class="card" style="width: 18rem;">
+				  	<img src="views/img/pri4.jpg" class="card-img-top" alt="...">
+				  	<div class="card-body">
+				    	<h5 class="card-title">Prof. Educación Física</h5>
+				    	<p class="card-text">Emanuel Mendez</p>
+				  	</div>
+				</div>
+			</div>
+		</div><br><br>
+		<!--------prof. primaria-------->
+		<!--------prof. secundaria-------->
+		<div class="row" align="center">
+			<div class="col-md-12">
+				<h3 align="center">Profesores nivel Primaria</h3><br>
+			</div>
+			<div class="col-lg-3">
+				<div class="card" style="width: 18rem;">
+				  	<img src="views/img/sec1.jpg" class="card-img-top" alt="...">
+				  	<div class="card-body">
+					    <h5 class="card-title">Prof. Matemáticas</h5>
+					    <p class="card-text">Rodolfo Cardozo</p>
+				  	</div>
+				</div>
+			</div>
+			<div class="col-lg-3">
+				<div class="card" style="width: 18rem;">
+				  	<img src="views/img/sec2.jpg" class="card-img-top" alt="...">
+				  	<div class="card-body">
+				    	<h5 class="card-title">Prof. Literatura</h5>
+				    	<p class="card-text">Jose Manuel Ticona</p>
+				  	</div>
+				</div>
+			</div>
+			<div class="col-lg-3">
+				<div class="card" style="width: 18rem;">
+				  	<img src="views/img/sec3.jpg" class="card-img-top" alt="...">
+				  	<div class="card-body">
+				    	<h5 class="card-title">Prof. Quimíca</h5>
+				    	<p class="card-text">Ramiro Sanches</p>
+				  	</div>
+				</div>
+			</div>
+			<div class="col-lg-3">
+				<div class="card" style="width: 18rem;">
+				  	<img src="views/img/sec4.jpg" class="card-img-top" alt="...">
+				  	<div class="card-body">
+				    	<h5 class="card-title">Prof. Física</h5>
+				    	<p class="card-text">Rimer Maldonado</p>
+				  	</div>
+				</div>
+			</div>
+		</div><br><br>
+		<!--------prof. secundaria-------->
+    </div><br><br>
+  </section>
