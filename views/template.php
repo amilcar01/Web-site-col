518c57a0abbf2c495d17f4ef4637733088afa73d
Rolando <rolando.pardo@uab.edu.bo>
2021-08-26 10:26:58 -0400

    modificacion del plantel docente
    modificación del plantel docente en cuanto al formato de presentación

--- views/template.php
+++ views/template.php
@@ -58,29 +58,12 @@
           <!-------seccion de actividades----->
           
           <!-------seccion de plantel docente---------> 
-           <section id="plantel" style="background-color: #F2F2F2;"><br><br><br>
-            <div class="container wow fadeInUp">
-              <div class="row">
-                <div class="col-md-12">
+          <?php 
+            include_once ("inclusiones/plantel.php");
+          ?>
+          <!-------seccion de plantel docente---------> 
 
-                  <hr class="featurette-divider">
-                  
-                  <h2>Plantel Docente</h2>
 
-                </div>
-              </div>
-            </div>
-            <div class="container about-container wow fadeInUp">
-              <div class="row">
-                <div class="col-md-12">
-                  <?php 
-                      include_once ("inclusiones/plantel.php");
-                    ?>
-                </div>
-              </div>
-            </div>
-          </section>
-          <!-------seccion de plantel docente--------->  
 
           <!-------seccion de galeria---------> 
            <section id="galeria"><br><br><br>
